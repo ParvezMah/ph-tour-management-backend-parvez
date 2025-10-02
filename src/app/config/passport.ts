e6055693b007988d7bc6c5be2599a044089eb436
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-02 14:46:22 +0600

    29-1 Configure Passport JS For Custom Authentication

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,9 +1,53 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import passport from "passport";
+import bcryptjs from "bcryptjs";
 import { Strategy as GoogleStrategy, Profile, VerifyCallback } from "passport-google-oauth20";
+import { Strategy as LocalStrategy } from "passport-local";
 import { envVars } from "./env";
 import { User } from "../modules/user/user.model";
 import { Role } from "../modules/user/user.interface";
+
+
+
+passport.use(
+    new LocalStrategy({
+        usernameField: "email",
+        passwordField: "password"
+    }, async (email: string, password: string, done) => {
+        try {
+            const isUserExist = await User.findOne({ email })
+
+            // if (!isUserExist) {
+            //     return done(null, false, { message: "User does not exist" })
+            // }
+
+            if (!isUserExist) {
+                return done("User does not exist")
+            }
+            const isPasswordMatched = await bcryptjs.compare(password as string, isUserExist.password as string)
+
+            if (!isPasswordMatched) {
+                return done(null, false, { message: "Password does not match" })
+            }
+            return done(null, isUserExist)
+            
+        } catch (error) {
+            console.log(error);
+            done(error)
+        }
+    })
+)
+
+
+
+
+
+
+
+
+
+
+
 
 
 passport.use(
