075296b09508667ca7217c4a2ef1a92343af19e0
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-03 09:06:42 +0600

    30-1 Create Slug during create and update of Division and Tour

--- src/app/modules/division/division.model.ts
+++ src/app/modules/division/division.model.ts
@@ -1,5 +1,5 @@
 import { model, Schema } from "mongoose";
-import { IDivision } from "./divishion.interface";
+import { IDivision } from "./division.interface";
 
 
 const divisionSchema = new Schema<IDivision>({
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
+import { DivisionRoutes } from "../modules/division/division.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 
 export const router = Router();
 
@@ -14,6 +16,14 @@ const moduleRoutes = [
         path: "/auth",
         route: AuthRoutes
     },
+    {
+        path: "/division",
+        route: DivisionRoutes
+    },
+    {
+        path: "/tour",
+        route: TourRoutes
+    }
 ]
 
 moduleRoutes.forEach((route)=>{
